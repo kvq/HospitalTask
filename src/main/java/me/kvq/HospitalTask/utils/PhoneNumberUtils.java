7fec2f399b030b6cd613cd6bf2ae72971cabfcf2
kvq <kvqplay@gmail.com>
2021-09-17 15:45:18 +0300

    if- style changes

--- src/main/java/me/kvq/HospitalTask/utils/PhoneNumberUtils.java
+++ src/main/java/me/kvq/HospitalTask/utils/PhoneNumberUtils.java
@@ -5,11 +5,17 @@ import me.kvq.HospitalTask.exception.InvalidPhoneNumberException;
 public class PhoneNumberUtils {
 
     public static String fixPhoneNumber(String number) throws InvalidPhoneNumberException {
-        if (number == null) return null;
+        if (number == null){
+            return null;
+        }
         number = number.replaceAll("[^\\d.]", "");
-        if (!number.startsWith("38")) number = "38" + number;
+        if (!number.startsWith("38")){
+            number = "38" + number;
+        }
         int length = number.length();
-        if (length < 12 || length > 13) throw new InvalidPhoneNumberException(number);
+        if (length < 12 || length > 13){
+            throw new InvalidPhoneNumberException(number);
+        }
         return number;
     }
 
